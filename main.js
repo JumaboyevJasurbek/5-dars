fabb73d7577dd52e0ca72a25a02d70dcd14b87e6
JumaboyevJasurbek <100076086+JumaboyevJasurbek@users.noreply.github.com>
2022-04-03 16:23:58 +0500

    5-dars

---
+++ main.js
@@ -0,0 +1,22 @@
+let yosh = Math.round(Math.random()*100);
+
+if ( yosh > 1 && yosh <= 7 ) {
+    console.log("Bog'cha yoshi" , `${yosh}`);
+
+} else if ( yosh >= 7 && yosh <= 18 )
+    console.log("Maktab yoshi" , `${yosh}`);
+
+else if ( yosh >= 18 && yosh <= 24 )
+console.log("universitet yoshi" , `${yosh}`);
+
+
+else if ( yosh >= 24 && yosh <= 40 )
+    console.log("Ishlash yoshi" , `${yosh}`);
+
+else if (yosh >= 40 && yosh <= 100 )
+    console.log("Nuroniy yoshi ", `${yosh}`);
+
+
+
+
+
